754d37f72205dac3baa8ca9b0ff02f30deed2d7e
remy0817 <451618054@qq.com>
2018-05-03 11:44:22 +0800

    新增MapTool的demo示例

---
+++ demo/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Demo/>
+  </div>
+</template>
+
+<script>
+import Demo from './components/Demo'
+
+export default {
+  name: 'App',
+  components: {
+    Demo
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
